2b486c7ffefd11e00b01729e58377ec6fad7066c
Mathieu Glachant <syvwlch@gmail.com>
2018-04-02 10:55:34 -0400

    Initial attempt at launching and landing an SRB with a chute on top

---
+++ HelloWorld.py
@@ -0,0 +1,41 @@
+# This is intendend for a simple rocket
+# with a non-detachable SRB & a chute
+
+import time
+import krpc
+conn = krpc.connect(name='Hello World')
+
+vessel = conn.space_center.active_vessel
+
+vessel.auto_pilot.target_pitch_and_heading(90, 90)
+vessel.auto_pilot.engage()
+vessel.control.throttle = 1
+time.sleep(1)
+
+print('Launch')
+vessel.control.activate_next_stage()
+
+fuel_amount = conn.get_call(vessel.resources.amount, 'SolidFuel')
+expr = conn.krpc.Expression.less_than(
+    conn.krpc.Expression.call(fuel_amount),
+    conn.krpc.Expression.constant_float(0.1))
+event = conn.krpc.add_event(expr)
+with event.condition:
+    event.wait()
+print('MECO')
+
+srf_altitude = conn.get_call(getattr, vessel.flight(), 'surface_altitude')
+expr = conn.krpc.Expression.less_than(
+    conn.krpc.Expression.call(srf_altitude),
+    conn.krpc.Expression.constant_double(1000))
+event = conn.krpc.add_event(expr)
+with event.condition:
+    event.wait()
+
+vessel.control.activate_next_stage()
+print('Deploying chutes')
+
+while vessel.flight(vessel.orbit.body.reference_frame).vertical_speed < -0.1:
+    print('Altitude = %.1f meters' % vessel.flight().surface_altitude)
+    time.sleep(1)
+print('Landed')
