3e4ea074b2037cd83a23225fabe64d1da0493a5a
Mathieu Glachant <syvwlch@gmail.com>
2018-04-09 22:59:20 -0400

    D'oh. Was starting burns 5 seconds early!

--- NodeExecutor.py
+++ NodeExecutor.py
@@ -79,6 +79,8 @@ burn_ut =  node.ut - (burn_time/2.)
 lead_time = 5
 update_UI('Warping to node')
 conn.space_center.warp_to(burn_ut - lead_time)
+while ut() < burn_ut:
+    pass
 
 # executing 98% of the burn
 update_UI('Executing burn')
