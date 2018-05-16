236c2b0b68ed2787214549e105707efa10fe9079
Mathieu Glachant <syvwlch@gmail.com>
2018-05-16 12:33:49 -0400

    Added more asserts to test_Hohmann_phase_angle. Should catch it if the calculations break.

--- test_HohmannTransfer.py
+++ test_HohmannTransfer.py
@@ -13,13 +13,31 @@ class TestCalculationFunctions(unittest.TestCase):
 
     def test_Hohmann_phase_angle(self):
         """Test the phase angle change during a Hohmann maneuver."""
-        SEMI_MAJOR_AXIS_1 = 100000
+        SEMI_MAJOR_AXIS = 164783
 
         self.assertAlmostEqual(
-            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS_1, SEMI_MAJOR_AXIS_1),
-            0.0,
-            7,
-            'Expected zero phase change when orbits are identical.',)
+            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS, SEMI_MAJOR_AXIS),
+            0.00,
+            2,
+            'Expected zero phase when orbits are identical.',)
+
+        self.assertAlmostEqual(
+            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS, 3*SEMI_MAJOR_AXIS),
+            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS/3, SEMI_MAJOR_AXIS),
+            2,
+            'Expected same result for given ratio of Semi Major Axis.')
+
+        self.assertAlmostEqual(
+            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS, 3*SEMI_MAJOR_AXIS),
+            116.36,
+            2,
+            'Expected 116 degree phase change when final orbit is 3x.')
+
+        self.assertAlmostEqual(
+            ht.Hohmann_phase_angle(SEMI_MAJOR_AXIS, 0.5*SEMI_MAJOR_AXIS),
+            -150.68,
+            2,
+            'Expected -151 degree phase change when final orbit is 0.5x.')
 
 
 if __name__ == '__main__':
