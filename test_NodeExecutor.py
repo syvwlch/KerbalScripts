e5b8c178ce5c611185fc925854d9212f7883a066
Mathieu Glachant <syvwlch@gmail.com>
2018-05-24 08:52:03 -0400

    Using named tuples to set up the mock objects
    Also adding setUp and tearDown methods.
    Working well so far.

--- test_NodeExecutor.py
+++ test_NodeExecutor.py
@@ -6,6 +6,7 @@ Work in progress.
 
 import unittest
 from unittest.mock import patch  # call
+from collections import namedtuple
 import sys
 import NodeExecutor
 
@@ -60,6 +61,76 @@ class Test_NodeExecutor_init(unittest.TestCase):
         except Exception as e:
             self.assertIsInstance(e, AssertionError)
         return
+
+
+@patch('krpc.connect', spec=True)
+class Test_NodeExecutor_ro_attributes(unittest.TestCase):
+    """
+    Test the NodeExecutor class read-only attributes.
+
+    Requires a patch on the KRPC server connection for:
+        - active vessel
+    """
+
+    def setUp(self):
+        """Set up the mock objects."""
+        node = namedtuple('node', 'delta_v ut')
+        self.node0 = node(delta_v=10, ut=20)
+        self.node1 = node(delta_v=30, ut=40)
+
+    def tearDown(self):
+        """Delete the mock objects."""
+        del(self.node0)
+        del(self.node1)
+
+    def test_node(self, mock_conn):
+        """Check that node is the first node from active vessel."""
+        with self.subTest(nodes=()):
+            mock_conn().space_center.active_vessel.control.nodes = ()
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.node, None)
+
+        with self.subTest('One node'):
+            mock_conn().space_center.active_vessel.control.nodes = (self.node0,)
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.node, self.node0)
+
+        with self.subTest('Two nodes'):
+            mock_conn().space_center.active_vessel.control.nodes = (self.node0, self.node1)
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.node, self.node0)
+
+    def test_has_node(self, mock_conn):
+        """Check that has_node is True only when the active vessel has at least one node."""
+        with self.subTest('No nodes'):
+            mock_conn().space_center.active_vessel.control.nodes = ()
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.has_node, False)
+
+        with self.subTest('One node'):
+            mock_conn().space_center.active_vessel.control.nodes = (self.node0,)
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.has_node, True)
+
+        with self.subTest('Two nodes'):
+            mock_conn().space_center.active_vessel.control.nodes = (self.node0, self.node1)
+            Hal9000 = NodeExecutor.NodeExecutor()
+            self.assertEqual(Hal9000.has_node, True)
+
+    def test_delta_v(self, mock_conn):
+        """Check that delta_v is set from the node."""
+        mock_conn().space_center.active_vessel.control.nodes = (self.node0,)
+        Hal9000 = NodeExecutor.NodeExecutor()
+        self.assertEqual(Hal9000.delta_v, self.node0.delta_v)
+
+    def test_burn_time_at_max_thrust(self, mock_conn):
+        """Check that burn_time_at_max_thrust is set from the node and active vessel."""
+        mock_conn().space_center.active_vessel.control.nodes = (self.node0,)
+        mock_conn().space_center.active_vessel.available_thrust = 10
+        mock_conn().space_center.active_vessel.specific_impulse = 20
+        mock_conn().space_center.active_vessel.mass = 30
+        Hal9000 = NodeExecutor.NodeExecutor()
+        self.assertEqual(Hal9000.burn_time_at_max_thrust, 10)
 
 
 if __name__ == '__main__':
